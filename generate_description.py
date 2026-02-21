f36f08e18462378f86d8ff5ca8654a79809f611c
xvshiting <597241336@qq.com>
2026-02-21 19:45:45 +0800

    update

--- generate_description.py
+++ generate_description.py
@@ -76,7 +76,7 @@ def generate_description(data):
     ]
     response = dashscope.Generation.call(
         # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx"
-        api_key=os.getenv('DASHSCOPE_API_KEY',"sk-b95c066723574c7ba72147797a568395"),
+        api_key=os.getenv('DASHSCOPE_API_KEY'),
         model="qwen-plus", # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
         messages=messages,
         result_format='message'
